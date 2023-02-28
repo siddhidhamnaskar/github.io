bad09ea132869f56236cc13d9155389bc1389207
Siddhi Dhamnaskar <siddhidhamnaskar64@gmail.com>
2023-02-28 18:40:41 +0530

    done

---
+++ portfolio/src/Components/navbar.jsx
@@ -0,0 +1,30 @@
+import "../App.css";
+// import Skills from "./skills"
+// import {Navigate} from "react";
+// import { Link } from "react-router-dom";
+
+
+export default function Navbar(){
+    return <>
+        <div id="navbar">
+         {/* <img src="https://scontent.fpnq1-1.fna.fbcdn.net/v/t39.30808-6/301862796_618648476633074_1728070246609855616_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=-qoBGxv9IUkAX9o0xUm&_nc_ht=scontent.fpnq1-1.fna&oh=00_AfB1fOAbpQHEJtICx_fjJU7k_xh1IcqCPetCGzUAPTiYLw&oe=639EC941" width="200px" height="100px" alt=""> --> */}
+        <p>Siddhi</p>
+        <div id="end-bar">
+            <a href="#aboutContainer"  >HOME</a>
+            <a href="#details"> ABOUT</a>
+             <a href="#skills">SKILLS</a>
+        
+          <a href="#projects">PROJECTS</a>
+           <a href="#contacts">CONTACTS</a>
+            
+           <div>
+            <a class="resume" href="https://drive.google.com/file/d/1ftv39LeqZwGB-Ee6f5ZGs6JJjz7ZuNmx/view?usp=sharing" target="_blank" rel="noreferrer">Resume<svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DownloadIcon"><path d="M5 20h14v-2H5v2zM19 9h-4V3H9v6H5l7 7 7-7z"></path></svg></a>
+           </div>
+       
+            
+
+        </div>
+ </div>
+
+    </>
+}
---
+++ portfolio/src/Components/skills.jsx
@@ -0,0 +1,32 @@
+
+export default function Skills(){
+    return <>
+
+       
+<div id="skills">
+        <h1>SKILLS</h1>
+        <div class="skillsContainer">
+            <div><img src="https://mohitkss.github.io/static/media/html5.4e6edfe05f07c383e94f.png" alt="HTML" width="70px" height="70px"/><p>HTML</p></div>
+            <div><img src="https://mohitkss.github.io/static/media/css3.845e8eb63836bef093cf.png" alt="CSS" width="70px" height="70px"/><p>CSS</p></div>
+            <div><img src="https://mohitkss.github.io/static/media/js.3dfb665e058d08c99f28.png" alt="Javascript" width="70px" height="70px"/><p>Javascript</p></div>
+            <div><img src="https://mohitkss.github.io/static/media/git.b1472a80b81e487179cf.png" alt="Git/Github" width="70px" height="70px"/><p>Git/Github</p></div>
+            <div><img src="https://mohitkss.github.io/static/media/mongodb.4f7af09e6354d51beec8.png" alt="MongoDB" width="70px" height="70px"/><p>MongoDB</p></div>
+            <div><img src="https://mohitkss.github.io/static/media/nodejs.b508473ad71a31ce2fae.png" alt="NodeJS" width="70px" height="70px"/><p>NodeJS</p></div>
+            <div><img src="https://mohitkss.github.io/static/media/react.22d1f2096ed82cab7a8a.png" alt="ReactJS" width="70px" height="70px"/><p>ReactJS</p></div>
+            <div><img src="https://mohitkss.github.io/static/media/redux.7fec6369cecd1cbd44d6.png" alt="Redux" width="70px" height="70px"/><p>Redux</p></div>
+             {/* <!-- <img alt="MERN" width="70px" height="70px"><p>MERN</p></div></div> --> */}
+       </div>
+    
+ </div>
+
+
+ </>
+
+
+
+    
+       
+
+    
+    
+}
