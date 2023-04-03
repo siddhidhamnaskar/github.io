7cd33354425f97f6e5b5268dd47250cb27d862ea
Siddhi Dhamnaskar <siddhidhamnaskar64@gmail.com>
2023-04-03 18:35:32 +0530

    calculator_added

--- portfolio/src/Components/navbar.jsx
+++ portfolio/src/Components/navbar.jsx
@@ -9,6 +9,7 @@ export default function Navbar(){
         <div id="navbar">
          {/* <img src="https://scontent.fpnq1-1.fna.fbcdn.net/v/t39.30808-6/301862796_618648476633074_1728070246609855616_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=-qoBGxv9IUkAX9o0xUm&_nc_ht=scontent.fpnq1-1.fna&oh=00_AfB1fOAbpQHEJtICx_fjJU7k_xh1IcqCPetCGzUAPTiYLw&oe=639EC941" width="200px" height="100px" alt=""> --> */}
         <p>Siddhi</p>
+       
         <div id="end-bar">
             <a href="#aboutContainer"  >HOME</a>
             <a href="#details"> ABOUT</a>
@@ -18,9 +19,9 @@ export default function Navbar(){
            <a href="#contacts">CONTACTS</a>
             
            <div>
-            <a class="resume" download href="pdf/Siddhi_Dhamnaskar_Resume.pdf" target="_blank" rel="noreferrer">Resume<svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DownloadIcon"><path d="M5 20h14v-2H5v2zM19 9h-4V3H9v6H5l7 7 7-7z"></path></svg></a>
+            <a class="resume" download href="pdf/Siddhi_Resume.pdf" >Resume<svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DownloadIcon"><path d="M5 20h14v-2H5v2zM19 9h-4V3H9v6H5l7 7 7-7z"></path></svg></a>
            </div>
-       
+          
             
 
         </div>
--- portfolio/src/Components/projects.jsx
+++ portfolio/src/Components/projects.jsx
@@ -1,6 +1,7 @@
 import blumercury from "../media/bluemercury.png";
 import ajio from "../media/ajio.png";
 import zoomcar from "../media/zoomcar.png";
+import calculator from "../media/calculator.png"
 export default function Project(){
     return<>
 
@@ -89,6 +90,36 @@ export default function Project(){
             </div>
           </div>
         </div>
+
+        
+
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <figure class="swing">
+                <img class="proimg" src={calculator} alt="" />
+              </figure>
+            </div>
+            <div class="flip-card-back">
+              <h1>CALCULATOR</h1>
+              <p>
+                 This is fully functional calculator made by me.
+              </p>
+              <p>Tech stack :- REACT.JS</p>
+              <div class="icons">
+                <a href="https://github.com/siddhidhamnaskar/calculator"
+                  ><svg id="githubsvg" class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="GitHubIcon"><path d="M12 1.27a11 11 0 00-3.48 21.46c.55.09.73-.28.73-.55v-1.84c-3.03.64-3.67-1.46-3.67-1.46-.55-1.29-1.28-1.65-1.28-1.65-.92-.65.1-.65.1-.65 1.1 0 1.73 1.1 1.73 1.1.92 1.65 2.57 1.2 3.21.92a2 2 0 01.64-1.47c-2.47-.27-5.04-1.19-5.04-5.5 0-1.1.46-2.1 1.2-2.84a3.76 3.76 0 010-2.93s.91-.28 3.11 1.1c1.8-.49 3.7-.49 5.5 0 2.1-1.38 3.02-1.1 3.02-1.1a3.76 3.76 0 010 2.93c.83.74 1.2 1.74 1.2 2.94 0 4.21-2.57 5.13-5.04 5.4.45.37.82.92.82 2.02v3.03c0 .27.1.64.73.55A11 11 0 0012 1.27"></path></svg></a>
+                <a href="https://siddhi-calculator.netlify.app/"
+                  ><svg id="livesvg" class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="StreamIcon"><circle cx="20" cy="12" r="2"></circle><circle cx="4" cy="12" r="2"></circle><circle cx="12" cy="20" r="2"></circle><path d="M10.05 8.59 6.03 4.55h-.01l-.31-.32-1.42 1.41 4.02 4.05.01-.01.31.32zm3.893.027 4.405-4.392L19.76 5.64l-4.405 4.393zM10.01 15.36l-1.42-1.41-4.03 4.01-.32.33 1.41 1.41 4.03-4.02zm9.75 2.94-3.99-4.01-.36-.35L14 15.35l3.99 4.01.35.35z"></path><circle cx="12" cy="4" r="2"></circle></svg>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
    
         </div>
 
